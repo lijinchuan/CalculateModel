c8c12ef5bc84df992434896acc8e06d360a08cc5
lijinchuan <403479851@qq.com>
2021-04-11 21:45:27 +0800

    解释器优化

---
+++ CalculateModel/StockFunction/Choose.cs
@@ -0,0 +1,33 @@
+﻿using LJC.FrameWork.CodeExpression;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATrade.CalculateModel.StockFunction
+{
+    internal class Choose:StockFun
+    {
+        public Choose(CalCurrent pool) : base(pool)
+        {
+
+        }
+
+        public override int Params => 0;
+
+        protected override CalResult CollectOperate()
+        {
+            return SingOperate();
+        }
+
+        protected override CalResult SingOperate()
+        {
+            return new CalResult
+            {
+                Result=this.CurrStockDataCalPool.Stock.StockCode,
+                ResultType=typeof(string)
+            };
+        }
+    }
+}
---
+++ CalculateModel/StockFunction/TradeTime.cs
@@ -0,0 +1,49 @@
+﻿using LJC.FrameWork.CodeExpression;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATrade.CalculateModel.StockFunction
+{
+    internal class TradeTime:StockFun
+    {
+        public TradeTime(CalCurrent pool) : base(pool)
+        {
+            
+        }
+
+        protected override CalResult CollectOperate()
+        {
+            if (CalCurrent.CurrentIndex > -1)
+            {
+                return new CalResult
+                {
+                    Result = this.CurrQuote.Time,
+                    ResultType = typeof(DateTime)
+                };
+            }
+
+            if (valueCach == null)
+            {
+                valueCach = CurrStockDataCalPool.Quotes.Select(q => (object)q.Time).ToArray();
+            }
+
+            return new CalResult
+            {
+                Results = valueCach,
+                ResultType = typeof(DateTime)
+            };
+        }
+
+        public override int Params => 0;
+
+        public override CalResult Operate()
+        {
+            return CollectOperate();
+        }
+
+        
+    }
+}

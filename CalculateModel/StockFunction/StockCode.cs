fee209d6f90c87cb8ab4631756b1857a2a31a5be
li.jinchuan <jinchuan.li@onesmart.org>
2021-06-04 14:15:49 +0800

    获取股票代码

---
+++ CalculateModel/StockFunction/StockCode.cs
@@ -0,0 +1,31 @@
+﻿using LJC.FrameWork.CodeExpression;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATrade.CalculateModel.StockFunction
+{
+    internal class StockCode : StockFun
+    {
+        public StockCode(CalCurrent pool) : base(pool)
+        {
+
+        }
+
+        protected override CalResult CollectOperate()
+        {
+            return SingOperate();
+        }
+
+        protected override CalResult SingOperate()
+        {
+            return new CalResult
+            {
+                Result = this.CurrStockDataCalPool.Stock.StockCode,
+                ResultType = typeof(string)
+            };
+        }
+    }
+}
